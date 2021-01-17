fc936cb8b6a68c37dfaf64c74796e0cf795c1bb8
Colin Daniels <colin.r.daniels@gmail.com>
2021-01-17 11:51:10 -0500

    Add fixed-width test based on OEIS A006840

--- tests/test_agnr.py
+++ tests/test_agnr.py
@@ -1,6 +1,7 @@
 from agnr_ml import AGNR, generate_all_possible_agnrs
 from unittest import TestCase
 from pymatgen import Lattice, Structure
+import pytest
 
 
 class TestAGNR(TestCase):
@@ -39,3 +40,15 @@ class TestAGNR(TestCase):
 
 def test_generation_doesnt_crash():
     generate_all_possible_agnrs(2, 2, 2, 2)
+
+
+# For fixed-width GNRs we expect the number of unique generated GNRs to be independent of width and follow
+# https://oeis.org/A006840 (Number of 2n-bead black-white reversible complementable necklaces with n black beads.)
+# since we essentially have two options for each hexagonal segment: shift up (white) or shift down (black) where the
+# reversible aspect is equivalent to an x mirror plane transformation, the complementable aspect to a y mirror plane,
+# and the necklace aspect is equivalent to taking into account all translations along the periodic axis.
+@pytest.mark.parametrize("length,num_expected", [(1, 1), (2, 2), (3, 3), (4, 7), (8, 257)])
+@pytest.mark.parametrize("width", [2, 3, 4, 8])
+def test_generation_constant_width(width: int, length: int, num_expected: int):
+    agnrs = generate_all_possible_agnrs(length, length, width, width)
+    assert len(list(agnrs)) == num_expected
